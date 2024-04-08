5103dda3d834171eaae44707f7e631df640e4dd8
yurikahirata <yurikahirata0130@gmail.com>
2024-04-08 23:31:54 +0900

    feat: image slider selection buttons

--- src/components/imageSlider.tsx
+++ src/components/imageSlider.tsx
@@ -1,5 +1,6 @@
 
 "use client"
+import { button } from "@material-tailwind/react";
 import { useState } from "react";
 
 type Props = {
@@ -32,6 +33,13 @@ export function ImageSlider({ imgUrls }: Props) {
       </div>
       <button onClick={handlePrevOnClick} className="absolute left-4 top-1/2"><span className="material-symbols-outlined">arrow_back_ios</span></button>
       <button onClick={handleNextOnClick} className="absolute right-3 top-1/2"><span className="material-symbols-outlined">arrow_forward_ios</span></button>
+      <div className="absolute left-1/2 bottom-2 flex gap-2">
+        {imgUrls.map((url, index) => (
+          <button key={index} onClick={() => setImgIndex(index)}>
+            {index === imgIndex ? <span className="material-symbols-outlined text-xs"> radio_button_checked</span> : <span className="material-symbols-outlined text-xs">radio_button_unchecked</span>}
+          </button>
+        ))}
+      </div>
     </section>
   )
 }
