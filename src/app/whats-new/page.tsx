627f24ed807989ebc64f10dc1160454f1928189d
yurikahirata <yurikahirata0130@gmail.com>
2024-04-22 20:34:44 +0900

    style: responsive pressContainer

--- src/app/whats-new/page.tsx
+++ src/app/whats-new/page.tsx
@@ -5,7 +5,7 @@ export default function WhatsNew() {
   return (
     <main className="mx-14 md:mx-20 lg:mx-36 mt-28 mb-16 min-h-lvh">
       <h1 className="text-center md:text-left">What's New</h1>
-      <iframe className="mt-6 aspect-video w-full" src="https://www.youtube.com/embed/0eA0s3H4aYE?si=fKgjVJpLaktIScoQ&amp;start=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      <iframe className="mt-6 aspect-video w-full" src="https://www.youtube.com/embed/0eA0s3H4aYE?si=fKgjVJpLaktIScoQ&amp;start=1" title="YouTube video player" allowFullScreen></iframe>
       <section className="mt-8">
         <h2>FACE A-J SS 2021</h2>
         <hr />
