46cbf56e5e1453caf3e796aa39ea15ab0da7c802
yurikahirata <yurikahirata0130@gmail.com>
2024-04-23 14:50:43 +0900

    fix: linting error apostrophe

--- src/app/whats-new/page.tsx
+++ src/app/whats-new/page.tsx
@@ -4,7 +4,7 @@ import { faceajss2020press, faceajss2021press } from "@/lib/pressData"
 export default function WhatsNew() {
   return (
     <main className="mx-14 md:mx-20 lg:mx-36 mt-28 mb-16 min-h-lvh">
-      <h1 className="text-center md:text-left">What's New</h1>
+      <h1 className="text-center md:text-left">What&#39;s New</h1>
       <iframe className="mt-6 aspect-video w-full" src="https://www.youtube.com/embed/0eA0s3H4aYE?si=fKgjVJpLaktIScoQ&amp;start=1" title="YouTube video player" allowFullScreen></iframe>
       <section className="mt-8">
         <h2>FACE A-J SS 2021</h2>
