937ed4d3737f342962a7f6411bd5118518686f93
yurikahirata <yurikahirata0130@gmail.com>
2024-05-22 16:17:36 +0900

    feat: next-intl setup, home page translation

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,14 @@
 // import Image from "next/image";
 'use client'
 import { useEffect } from "react";
-import styles from "../styles/home.module.css";
+import styles from "/src/styles/home.module.css";
 import { profiles } from "@/lib/teamData";
 import { ProfileCard } from "@/components/profileCard";
 import Image from "next/image";
+import { useTranslations } from 'next-intl';
 
 export default function Home() {
+  const t = useTranslations("Home");
 
   useEffect(() => {
     const header = document.querySelector("#header");
@@ -54,12 +56,12 @@ export default function Home() {
         <img src="/placeholder_hero.png" alt="two models on a runway" className="h-full w-full object-cover" />
       </section>
       <section className="mx-14 md:mx-20 lg:mx-36 my-12">
-        <h1 className="text-center md:text-left">About Us</h1>
-        <p className="mt-4">Awa’Tori meaning “Our Story” in Pidgin English (widely spoken language across anglophone African countries), and in Japanese, loosely translated as Awa – 合わせ meaning bringing several cultures together and Tori – 通り as in bridging the African and Japanese creative markets. <br /><br />
-          Founded in 2018 by Bukky Adejobi and Seiko Mbako, Awa’Tori is a platform that is passionate about the African and Japanese creative industry. We believe the creatives in the African and Asian markets have stories worth telling that are expressed in the form of fashion, arts, music, or film. Cut off from certain parts of the world, we believe it is our responsibility to help them tell their stories beyond their respective borders. <br /><br />
-          With the overarching mission to contribute socioeconomic development in Africa and Japan, Bukky and Seiko, both of African descent, believe in the power of such a synergy. Through partnerships with stakeholders from the supply chain and collaborations with third part organizations, Awa’Tori aims to develop and facilitate projects that foster sustainable partnerships that bridges the gap between the Creative Industries of Africa and Japan.
+        <h1 className="text-center md:text-left">{t("about-title")}</h1>
+        <p className="mt-4">{t("about1")} <br /><br />
+          {t("about2")} <br /><br />
+          {t("about3")}
         </p>
-        <h1 className="mt-12 text-center md:text-left">Our Team</h1>
+        <h1 className="mt-12 text-center md:text-left">{t("team")}</h1>
         <div className="flex flex-col mt-8 gap-6 mb-24">
           {profiles.map((profile) => (
             <ProfileCard key={profile.name} profile={profile} />
