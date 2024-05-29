031eec0b0236b34d224e9df630e7094b6bcb2eb1
yurikahirata <yurikahirata0130@gmail.com>
2024-05-29 12:24:48 +0900

    feat: footer translations

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,18 @@
 import Link from "next/link";
+import { useTranslations } from "next-intl";
+
 export function Footer() {
+  const t = useTranslations("Menu");
+
   return (
     <footer className="flex flex-col px-8 py-5 text-white w-full bg-black px-14">
       <div className="flex flex-col md:flex-row justify-between items-center">
         <img src="/logos/awatori_logo_white.png" alt="Awa'Tori logo in white" className="w-16" />
         <nav className="flex flex-col text-center md:flex-row md:text-left justify-between gap-x-6 text-sm">
-          <Link href="/" className="hover:underline mt-2 md:mt-0">Home</Link>
-          <Link href="/projects" className="hover:underline mt-2 md:mt-0">Projects</Link>
-          <Link href="/our-creatives" className="hover:underline mt-2 md:mt-0">Our Creatives</Link>
-          <Link href="/contact-us" className="hover:underline mt-2 md:mt-0">Contact Us</Link>
+          <Link href="/" className="hover:underline mt-2 md:mt-0">{t("home")}</Link>
+          <Link href="/projects" className="hover:underline mt-2 md:mt-0">{t("projects")}</Link>
+          <Link href="/our-creatives" className="hover:underline mt-2 md:mt-0">{t("creatives")}</Link>
+          <Link href="/contact-us" className="hover:underline mt-2 md:mt-0">{t("contact")}</Link>
         </nav>
         <nav className="flex gap-3 mt-4 md:mt-0">
           <a href="https://www.youtube.com/channel/UCHCt0lehc9PLV7JS3Ytlkag/featured?view_as=subscriber" target="_blank"><img src="/icons/youtube_icon.png" alt="YouTube icon" className="w-8" /></a>
